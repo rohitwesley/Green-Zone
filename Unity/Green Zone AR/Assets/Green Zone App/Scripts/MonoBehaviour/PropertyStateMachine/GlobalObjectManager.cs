0aac0fedaa682520dd894467627a9a82a821b065
Diego Kaleb Valenzuela Carrillo <a01018992@itesm.mx>
2020-07-09 18:58:43 -0700

    Cleaning

--- Unity/Green Zone AR/Assets/Green Zone App/Scripts/MonoBehaviour/PropertyStateMachine/GlobalObjectManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GlobalObjectManager : MonoBehaviour
-{
-    static protected int _PickUpsCount = 0;
-    static protected int _PickUpsTotal = 0;
-}
